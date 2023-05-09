dcf6ff45ed586cee08e15f93cc841da34590a29b
Ramil <ramiltux@mail.ru>
2023-05-09 16:22:18 +0300

    Разработка бизнес логики приложения

---
+++ Interface1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laba05041
+{
+    public interface Interface1
+    {
+        void Excute(object par);
+        bool CanExcute(object par);
+        event EventHandler CanExcuteChanged;
+    }
+}
